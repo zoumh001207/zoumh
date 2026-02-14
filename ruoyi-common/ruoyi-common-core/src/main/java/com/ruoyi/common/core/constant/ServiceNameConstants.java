9e7d9e0519d2b5d6e01e3080ef6f05436ff65160
zoumh <1786291497@qq.com>
2026-02-15 00:06:01 +0800

    1

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/constant/ServiceNameConstants.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/constant/ServiceNameConstants.java
@@ -21,4 +21,9 @@ public class ServiceNameConstants
      * 文件服务的serviceid
      */
     public static final String FILE_SERVICE = "ruoyi-file";
+
+    /**
+     * 内部请求来源
+     */
+    public static final String FROM_SOURCE = "from-source";
 }
