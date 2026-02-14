c414209124eed6cc7b606dbea22969e85675d153
zoumh <1786291497@qq.com>
2026-02-15 00:06:09 +0800

    1

---
+++ zoumh-modules/zoumh-tools/src/main/java/com/zoumh/ToolsApplication.java
@@ -0,0 +1,24 @@
+package com.zoumh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsApplication.class, args);
+        System.out.println(">>万人服务联盟-平台核心业务模块启动成功  \n" +
+                " ___                         ,--,               \n" +
+                "  ,--.'|_                     ,--.'|               \n" +
+                "  |  | :,'   ,---.     ,---.  |  | :               \n" +
+                "  :  : ' :  '   ,'\\   '   ,'\\ :  : '    .--.--.    \n" +
+                ".;__,'  /  /   /   | /   /   ||  ' |   /  /    '   \n" +
+                "|  |   |  .   ; ,. :.   ; ,. :'  | |  |  :  /`./   \n" +
+                ":__,'| :  '   | |: :'   | |: :|  | :  |  :  ;_     \n" +
+                "  '  : |__'   | .; :'   | .; :'  : |__ \\  \\    `.  \n" +
+                "  |  | '.'|   :    ||   :    ||  | '.'| `----.   \\ \n" +
+                "  ;  :    ;\\   \\  /  \\   \\  / ;  :    ;/  /`--'  / \n" +
+                "  |  ,   /  `----'    `----'  |  ,   /'--'.     /  \n" +
+                "   ---`-'                      ---`-'   `--'---'");
+    }
+}
