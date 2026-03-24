e91279b22a6b38cf565b66f8369361a71e692452
zoumh <1786291497@qq.com>
2026-03-24 21:21:01 +0800

    refactor: remove unused business modules

--- zoumh-modules/zoumh-hotel-monitor/src/main/java/com/zoumh/hotelmonitor/service/HotelPriceCrawlerService.java
+++
@@ -1,9 +0,0 @@
-package com.zoumh.hotelmonitor.service;
-
-import com.zoumh.hotelmonitor.domain.CrawlExecutionResult;
-import com.zoumh.hotelmonitor.domain.HotelPriceMonitor;
-
-public interface HotelPriceCrawlerService {
-
-    CrawlExecutionResult crawl(HotelPriceMonitor monitor);
-}
